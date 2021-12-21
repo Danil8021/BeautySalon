9d7f6654426cdaef2f8ec95dc82e56804bf1829c
Danil8021 <chankov02.02@mail.ru>
2021-12-21 12:38:12 +0700

    Добавьте файлы проекта.

---
+++ BeautySalon/ClientsList.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BeautySalon
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        BeautySalonEntities context;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            context = new BeautySalonEntities();
+            ShowTClient();
+        }
+        
+        public void ShowTClient()
+        {
+            dgClient.ItemsSource = context.Client.ToList();
+        }
+
+        private void dgClient_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
